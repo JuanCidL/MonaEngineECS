50a6bb0971559a20dca1af0a54eed1a25d4b38a5
IgnacioGHS <101026582+IgnacioGHS@users.noreply.github.com>
2024-12-15 14:04:10 -0300

    Feat: Stats struct created, applied to ECSTest and tested.

--- examples/ECSExample.cpp
+++ examples/ECSExample.cpp
@@ -6,36 +6,40 @@ class MinimalSetup : public Mona::Application
 public:
 	MinimalSetup() = default;
 	~MinimalSetup() = default;
-	virtual void UserStartUp(Mona::World &world) noexcept override{
+	virtual void UserStartUp(Mona::World &world) noexcept override
+	{
 		// statsSystem.StartUp(componentManager, eventManager);
 		auto e1 = componentManager.CreateEntity();
 		auto e2 = componentManager.CreateEntity();
 		auto e3 = componentManager.CreateEntity();
 
 		componentManager.DestroyEntity(e1);
-		// componentManager.DestroyEntity(e2);
+		componentManager.DestroyEntity(e2);
 		// componentManager.DestroyEntity(e3);
-		Mona::Comp& uw = componentManager.AddComponent<Mona::Comp>(e3, 10.0f);
-		Mona::Comp2& uw2 = componentManager.AddComponent<Mona::Comp2>(e3, 10.0f);
+		// Mona::Comp &uw = componentManager.AddComponent<Mona::Comp>(e3, 10.0f);
+		// Mona::Comp2 &uw2 = componentManager.AddComponent<Mona::Comp2>(e3, 10.0f);
 
-		Mona::Comp& uw4 = componentManager.AddComponent<Mona::Comp>(e2, 10.0f);
+		// Mona::Comp &uw4 = componentManager.AddComponent<Mona::Comp>(e2, 10.0f);
+
+		Mona::Stats &st = componentManager.AddComponent<Mona::Stats>(e3);
 
 		systemManager.RegisterSystem<Mona::StatsEnTTSystem>();
 		systemManager.StartUpSystems(componentManager, eventManager);
 	}
 
-	virtual void UserShutDown(Mona::World& world) noexcept override {
+	virtual void UserShutDown(Mona::World &world) noexcept override
+	{
 		// statsSystem.ShutDown(componentManager, eventManager);
 		systemManager.ShutDownSystems(componentManager, eventManager);
 	}
-	virtual void UserUpdate(Mona::World& world, float timeStep) noexcept override {
+	virtual void UserUpdate(Mona::World &world, float timeStep) noexcept override
+	{
 		// statsSystem.Update(componentManager, eventManager, timeStep);
 		// auto count = componentManager.GetComponentCount<Mona::Comp>();
 		// auto count2 = componentManager.GetComponentCount<Mona::Comp2>();
 		// std::cout << "Comp count: " << count << std::endl;
 		// std::cout << "Comp2 count: " << count2 << std::endl;
 		systemManager.UpdateSystems(componentManager, eventManager, timeStep);
-		
 	}
 
 private:
@@ -45,9 +49,8 @@ private:
 	Mona::StatsEnTTSystem statsSystem;
 };
 int main()
-{	
+{
 	MinimalSetup app;
 	Mona::Engine engine(app);
 	engine.StartMainLoop();
-	
 }
--- source/ECS/Components/StatComponents.hpp
+++ source/ECS/Components/StatComponents.hpp
@@ -1,15 +1,34 @@
 #pragma once
 #ifndef STAT_COMPONENTS_HPP
 #define STAT_COMPONENTS_HPP
+#include <glm/glm.hpp>
 
-namespace Mona 
+namespace Mona
 {
-    struct Comp {
-        float x = 0.0f;
+    enum class StatsColors
+    {
+        red,
+        green,
+        blue
     };
+    struct Stats
+    {
+        float health = 100.0f;
+        float speed = 10.0f;
+        StatsColors state = StatsColors::red;
 
-    struct Comp2 {
-        float x = 0.0f;
+        glm::vec3 getColor() // tengo la duda con que esto esté acá
+        {
+            switch (state)
+            {
+            case StatsColors::red:
+                return glm::vec3(1.0f, 0.0f, 0.0f);
+            case StatsColors::green:
+                return glm::vec3(0.0f, 1.0f, 0.0f);
+            case StatsColors::blue:
+                return glm::vec3(0.0f, 0.0f, 1.0f);
+            }
+        }
     };
 }
 
