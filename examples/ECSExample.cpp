996b72f1d4c9c96bcccbb9d06f547853a3425291
JuanCidL <jlcl.juancid@gmail.com>
2024-12-16 00:11:43 -0300

    feat: event manager features implemented. Subcribe, Unsubscribe and Publish events and callback functions

--- examples/ECSExample.cpp
+++ examples/ECSExample.cpp
@@ -1,6 +1,27 @@
 #include "MonaEngine.hpp"
 #include "ECS/ECS.hpp"
 #include "ECS/Systems/StatsEnTTSystem.hpp"
+
+struct TestEvent
+{
+	int value;
+};
+void func(const TestEvent &event)
+{
+	std::cout << "Event value: " << event.value << std::endl;
+}
+
+class ExampleClassUsingEvent
+{
+public:
+	ExampleClassUsingEvent() = default;
+	~ExampleClassUsingEvent() = default;
+	void OnEvent(const TestEvent &event)
+	{
+		std::cout << "Event value: " << event.value << std::endl;
+	}
+};
+
 class MinimalSetup : public Mona::Application
 {
 public:
@@ -8,38 +29,37 @@ public:
 	~MinimalSetup() = default;
 	virtual void UserStartUp(Mona::World &world) noexcept override
 	{
-		// statsSystem.StartUp(componentManager, eventManager);
 		auto e1 = componentManager.CreateEntity();
 		auto e2 = componentManager.CreateEntity();
 		auto e3 = componentManager.CreateEntity();
 
 		componentManager.DestroyEntity(e1);
 		componentManager.DestroyEntity(e2);
-		// componentManager.DestroyEntity(e3);
-		// Mona::Comp &uw = componentManager.AddComponent<Mona::Comp>(e3, 10.0f);
-		// Mona::Comp2 &uw2 = componentManager.AddComponent<Mona::Comp2>(e3, 10.0f);
-
-		// Mona::Comp &uw4 = componentManager.AddComponent<Mona::Comp>(e2, 10.0f);
 
 		Mona::Stats &st = componentManager.AddComponent<Mona::Stats>(e3);
 
+
+		eventManager.Subscribe<TestEvent, &func>();
+		eventManager.Subscribe<TestEvent, ExampleClassUsingEvent, &ExampleClassUsingEvent::OnEvent>(exampleClass);
 		systemManager.RegisterSystem<Mona::StatsEnTTSystem>();
 		systemManager.StartUpSystems(componentManager, eventManager);
 	}
 
 	virtual void UserShutDown(Mona::World &world) noexcept override
 	{
-		// statsSystem.ShutDown(componentManager, eventManager);
 		systemManager.ShutDownSystems(componentManager, eventManager);
 	}
 	virtual void UserUpdate(Mona::World &world, float timeStep) noexcept override
 	{
-		// statsSystem.Update(componentManager, eventManager, timeStep);
-		// auto count = componentManager.GetComponentCount<Mona::Comp>();
-		// auto count2 = componentManager.GetComponentCount<Mona::Comp2>();
-		// std::cout << "Comp count: " << count << std::endl;
-		// std::cout << "Comp2 count: " << count2 << std::endl;
 		systemManager.UpdateSystems(componentManager, eventManager, timeStep);
+		time += timeStep;
+		if (time > 1.0f)
+		{
+			TestEvent event;
+			event.value = 10;
+			eventManager.Publish(event);
+			time = 0.0f;
+		}
 	}
 
 private:
@@ -47,6 +67,8 @@ private:
 	Mona::EnTTEventManager eventManager;
 	Mona::EnTTSystemManager systemManager;
 	Mona::StatsEnTTSystem statsSystem;
+	ExampleClassUsingEvent exampleClass;
+	float time = 0.0f;
 };
 int main()
 {
--- source/ECS/EnTTEventManager.hpp
+++ source/ECS/EnTTEventManager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #ifndef ENTT_EVENT_MANAGER_HPP
 #define ENTT_EVENT_MANAGER_HPP
+#include <entt/entt.hpp>
+#include <functional>
 
 namespace Mona
 {
@@ -8,6 +10,42 @@ namespace Mona
     {
     public:
         EnTTEventManager() = default;
+        ~EnTTEventManager() = default;
+
+        // Event subscription using a function pointer
+        template<typename EventType, void(*func)(const EventType &)>
+        void Subscribe()
+        {
+            m_dispatcher.sink<EventType>().template connect<func>();
+        }
+
+        // Event subscription using a member function pointer
+        template<typename EventType, typename ObjectType, void(ObjectType::*func)(const EventType &)>
+        void Subscribe(ObjectType& obj)
+        {
+            m_dispatcher.sink<EventType>().template connect<func>(obj);
+        }
+
+        template<typename EventType>
+        void Publish(const EventType& event)
+        {
+            m_dispatcher.trigger(event);
+        }
+
+        template<typename EventType, void(*func)(const EventType &)>
+        void Unsubscribe()
+        {
+            m_dispatcher.sink<EventType>().template disconnect<func>();
+        }
+
+        template<typename EventType, typename ObjectType, void(ObjectType::*func)(const EventType &)>
+        void Unsubscribe(ObjectType& obj)
+        {
+            m_dispatcher.sink<EventType>().template disconnect<func>(obj);
+        }
+        
+    private:
+        entt::dispatcher m_dispatcher{};
     };   
 }
 
